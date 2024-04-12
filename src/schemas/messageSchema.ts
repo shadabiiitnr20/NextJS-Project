4cf2edb24c5a08ff88104455ace45b069b8a01a6
Mohammad Shadab Hussain <shadab.iiitnr20@gmail.com>
2024-04-12 17:30:29 +0530

    saved messageSchema file

--- src/schemas/messageSchema.ts
+++ src/schemas/messageSchema.ts
@@ -4,5 +4,5 @@ export const messageSchema = z.object({
   content: z
     .string()
     .min(10, { message: "Content must be of at least 10 characters" })
-    .max(300, { message: "Content must no longer be than 300 characters" }),
+    .max(300, { message: "Content must no longer be more than 300 characters" }),
 });
